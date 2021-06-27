c27768f5a011cf9dc70280f2253e81f1903856d1
William Emerison Six <billsix@gmail.com>
2021-06-27 05:18:20 -0400

    updated to newest kernel

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 qemu-system-mips -M malta \
   -m 1024 -hda hda.img \
-  -kernel vmlinux-4.19.0-16-4kc-malta \
-  -initrd initrd.img-4.19.0-16-4kc-malta \
+  -kernel vmlinux-4.19.0-17-4kc-malta \
+  -initrd initrd.img-4.19.0-17-4kc-malta \
   -append "root=/dev/sda1 console=ttyS0 nokaslr" \
   -nographic \
   -net user,hostfwd=tcp::10022-:22 \
